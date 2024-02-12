974b9ffd63326112b4c736d529ea51ed06ba2cb0
minji0916 <77606943+minji0916@users.noreply.github.com>
2024-02-12 12:00:29 +0900

    Time: 148 ms (24.02%) | Memory: 17.9 MB (83.33%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,6 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         dicts = {}
 
+        # 123
         for num in nums:
             if num in dicts:
                 dicts[num] += 1
