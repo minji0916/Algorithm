7e73452bfadb185f21a7e690f5297256213265e1
minji0916 <77606943+minji0916@users.noreply.github.com>
2024-08-06 00:08:25 +0900

    [Silver V] Title: 배열 합치기, Time: 1012 ms, Memory: 305796 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -5,7 +5,7 @@ N,M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-C = A+B
-C.sort()
+C = sorted(A+B)
+
 answer = ' '.join(map(str, C))
 print(answer)
